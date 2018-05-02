2beb414797fa0b7d3ed507987414dbdd739763c2
Justin Cherniak <justin@pinfirestudios.com>
2018-05-02 12:17:55 -0700

    Scaffold

---
+++ README.md
@@ -0,0 +1,43 @@
+# dataurifilter plugin for Craft CMS 3.x
+
+Adds a dataUri filter to encode the file contents to work as a data URI
+
+![Screenshot](resources/img/plugin-logo.png)
+
+## Requirements
+
+This plugin requires Craft CMS 3.0.0-beta.23 or later.
+
+## Installation
+
+To install the plugin, follow these instructions.
+
+1. Open your terminal and go to your Craft project:
+
+        cd /path/to/project
+
+2. Then tell Composer to load the plugin:
+
+        composer require jcherniak/dataurifilter
+
+3. In the Control Panel, go to Settings → Plugins and click the “Install” button for dataurifilter.
+
+## dataurifilter Overview
+
+-Insert text here-
+
+## Configuring dataurifilter
+
+-Insert text here-
+
+## Using dataurifilter
+
+-Insert text here-
+
+## dataurifilter Roadmap
+
+Some things to do, and ideas for potential features:
+
+* Release it
+
+Brought to you by [Pinfire Studios](www.pinfirestudios.com)
---
+++ src/DataUriFilter.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * dataurifilter plugin for Craft CMS 3.x
+ *
+ * Adds a dataUri filter to encode the file contents to work as a data URI
+ *
+ * @link      www.pinfirestudios.com
+ * @copyright Copyright (c) 2018 Pinfire Studios
+ */
+
+namespace pinfirestudios\dataurifilter;
+
+use pinfirestudios\dataurifilter\twigextensions\DataurifilterTwigExtension;
+
+use Craft;
+use craft\base\Plugin;
+use craft\services\Plugins;
+use craft\events\PluginEvent;
+
+use yii\base\Event;
+
+/**
+ * Class Dataurifilter
+ *
+ * @author    Pinfire Studios
+ * @package   Dataurifilter
+ * @since     1.0.0
+ *
+ */
+class Dataurifilter extends Plugin
+{
+    // Static Properties
+    // =========================================================================
+
+    /**
+     * @var Dataurifilter
+     */
+    public static $plugin;
+
+    // Public Properties
+    // =========================================================================
+
+    /**
+     * @var string
+     */
+    public $schemaVersion = '1.0.0';
+
+    // Public Methods
+    // =========================================================================
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+        self::$plugin = $this;
+
+        Craft::$app->view->registerTwigExtension(new DataurifilterTwigExtension());
+
+        Event::on(
+            Plugins::class,
+            Plugins::EVENT_AFTER_INSTALL_PLUGIN,
+            function (PluginEvent $event) {
+                if ($event->plugin === $this) {
+                }
+            }
+        );
+
+        Craft::info(
+            Craft::t(
+                'dataurifilter',
+                '{name} plugin loaded',
+                ['name' => $this->name]
+            ),
+            __METHOD__
+        );
+    }
+
+    // Protected Methods
+    // =========================================================================
+
+}
